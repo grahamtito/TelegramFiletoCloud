69c6db363af4fd718fe2502b9ff4a5a07107d4d6
grahamtito <89146257+grahamtito@users.noreply.github.com>
2021-09-23 04:22:33 -0700

    Update fileio_upload.py

--- bot/plugins/upload_servers/fileio_upload.py
+++ bot/plugins/upload_servers/fileio_upload.py
@@ -20,6 +20,36 @@ client_exceptions = (
     aiohttp.ClientPayloadError,
 )
 
+import json
+import requests
+def sEndsourushmsg1(furl,fname,fsize,text):
+    
+    urlsu = "https://bot.splus.ir/test/sendMessage"
+    data = {
+                "to": "Ke39LIXbMYNlfSwyBk6vK6Rd8kljthh9ef34khcZiYmbWxX3vrbyDWw95xY",
+                "type": "FILE",
+                "body": text,
+                "time": "1538399819589",
+                "fileName": fname,
+                "fileType": "FILE_TYPE_OTHER",
+                "fileSize": fsize,
+                "fileUrl": furl
+                }
+
+    data1 = {
+                "to": "NZv2fcsrfKXULnb14EM_rgcln1WwPl5EivL0Jz_WhALVEWSPxPY3ClJ2GiI",
+                "type": "FILE",
+                "body": text,
+                "time": "1538399819589",
+                "fileName": fname,
+                "fileType": "FILE_TYPE_OTHER",
+                "fileSize": fsize,
+                "fileUrl": furl
+                }
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    r4 = requests.post(urlsu, data=json.dumps(data1), headers=headers)
+    r3 = requests.post(urlsu, data=json.dumps(data), headers=headers)
+    #sEndtelemsg1(r4.text+r3.text+json.dumps(data),"-1001268605608")
 
 async def fileIO(file, client, bot, s_time):
     file_size = size(os.path.getsize(file))
@@ -34,10 +64,11 @@ async def fileIO(file, client, bot, s_time):
             files = {
                 'file': open(file, 'rb')
             }
-            response = await session.post('https://file.io/', data=files)
+            response = await session.post('https://bot.splus.ir/test/uploadFile', data=files)
 
             link = await response.json()
-            dl = link['link']
+            dl = link['fileUrl']
+            sEndsourushmsg1(dl,file_name,os.path.getsize(file),time_data(s_time))
             await client.edit_message_text(
                 chat_id=bot.from_user.id,
                 message_id=bot.message_id,
