a8d116cc6bdf8015d59e9c9c8ffe4c8bd4161793
grahamtito <89146257+grahamtito@users.noreply.github.com>
2021-09-23 04:24:18 -0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
 class Config:
-    BOT_TOKEN = ''                                                  # from @botfather
+    BOT_TOKEN = '1304663311:AAGStDkLpKkYFNf73wivgjXmvnca5eESxY8'                                                  # from @botfather
     APP_ID =                                                        # from https://my.telegram.org/apps
     API_HASH = ''                                                   # from https://my.telegram.org/apps
