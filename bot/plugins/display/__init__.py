cbef386c7b5d3bc62c8e56371d61e581a4c1edc2
grahamtito <89146257+grahamtito@users.noreply.github.com>
2021-10-14 20:08:21 +0330

    Update __init__.py

--- bot/plugins/display/__init__.py
+++ bot/plugins/display/__init__.py
@@ -6,13 +6,19 @@
 
 
 from bot.plugins.display.time import time_data
+import time
+import asyncio
 
 async def progress(current, total, up_msg, message, start_time):
+    
 
     try:
-        await message.edit(
-            text=f"{up_msg} {current * 100 / total:.1f}% in {time_data(start_time)}"
-        )
+        diff = int(time.time() - start_time)
+        if (int(time.time()) % 5 == 0) or (cur == tot):
+            await asyncio.sleep(2)
+            await message.edit(
+                 text=f"{up_msg} {current * 100 / total:.1f}% in {time_data(start_time)}"
+                )
     except Exception as e:
         await message.edit(
             text=f"ERROR: {e}"
